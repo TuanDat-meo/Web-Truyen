695a7b23c792f46cef1af1e8fccf9330b92d5f70
Đạt Nguyễn Hoàng Tuấn <6451071014@st.utc2.edu.vn>
2024-11-07 16:09:41 +0700

    upload

--- JS/slide.js
+++ JS/slide.js
@@ -1,6 +1,5 @@
 let currentSlideIndex = 0; // Biến theo dõi chỉ số của slider hiện tại
 let totalSlides = document.querySelectorAll('.slider-item').length; // Tổng số slide (cả hai slider có cùng số lượng item)
-
 // Hàm chuyển đến slide kế tiếp
 function showNext() {
     if (currentSlideIndex < totalSlides - 1) {
@@ -10,7 +9,6 @@ function showNext() {
     }
     updateSlider(); // Cập nhật vị trí của cả hai sliders
 }
-
 // Hàm chuyển đến slide trước đó
 function showPrevious() {
     if (currentSlideIndex > 0) {
@@ -20,14 +18,11 @@ function showPrevious() {
     }
     updateSlider(); // Cập nhật vị trí của cả hai sliders
 }
-
 // Hàm cập nhật vị trí của sliders
 function updateSlider() {
     let translateValue = -currentSlideIndex * 100; // Giá trị dịch chuyển theo phần trăm
-
     // Dịch chuyển slider 1
     document.querySelector('.slider-top-show').style.transform = `translateX(${translateValue}%)`;
-
     // Dịch chuyển slider 2
     document.querySelector('.slider-bottom-show').style.transform = `translateX(${translateValue}%)`;
 }
